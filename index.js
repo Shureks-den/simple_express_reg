da421c5dabe5c6f80daf3932c4b4876e788b9f78
Alexander Klonov <alexander.klonov@mail.ru>
2022-07-22 15:49:18 +0300

    Added auth cookie

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-console.log('hello')
+
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 const app = express();
 const port = 8000;
+
 app.use(express.json());
+app.use(cookieParser());
 
 const users = [
     {
@@ -11,8 +16,17 @@ const users = [
     },
 ];
 
+const cookieOptions = {
+    maxAge: 1000 * 60 * 15, //15 min
+    httpOnly: true,
+}
+
 app.get('/users', async (req, res) => {
     try {
+        if (req.cookies.authCookie === undefined || !users.find(u => u.token === req.cookies.authCookie)) {
+            res.status(403).json({data: {message: "No cookie"}});
+            return;
+        }
         res.json(users.map(u => u.login));
     } catch (e) {
         res.json(e);
@@ -23,24 +37,29 @@ app.post('/auth', async (req, res) => {
     const {login, password} = req.body;
     const foundUser = users.find(u => u.login === login);
     if (!foundUser) { 
-        res.json({status: 404, statusText: "Not Found", data: {message: "No such user found"}});
+        res.status(404).json({data: {message: "No such user found"}});
         return;
     };
     if (foundUser.password !== password) {
         res.status(403).json({data: {message: "Wrong password"}});
         return;
     }
-    res.json(foundUser)
+    const token = uuidv4();
+    foundUser.token = token;
+    res.cookie('authCookie', token, cookieOptions);
+    res.json(req.body)
 });
 
 app.post('/reg', async (req, res) => {
-    const {login} = req.body;
+    const {login, password} = req.body;
     const foundUser = users.find(u => u.login === login);
     if (foundUser)  {
         res.status(409).json({data: {message: "User already exists"}})
         return;
     };
-    users.push(req.body);
+    const token = uuidv4();
+    users.push({...req.body, token: token});
+    res.cookie('authCookie', token, cookieOptions);
     res.json(req.body);
 });
 
